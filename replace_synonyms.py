c9dc54829dc5c17c0209fd25802bda2d18302565
Kyle Troutner <kyletroutner1@gmail.com>
2014-02-28 15:44:29 -0500

    fixed merge issues

--- replace_synonyms.py
+++ replace_synonyms.py
@@ -1,13 +1,10 @@
 import sys, os, os.path
 from nltk.corpus import wordnet
-<<<<<<< HEAD
 from nltk.tokenize.punkt import PunktWordTokenizer
 import sys
 import os
 import os.path
 import string
-=======
->>>>>>> f023839b7463e65b4a15b7ab293cfd1f846c4941
 
 # Given a filename, opens the file and returns it's text
 def read_file(filename):
@@ -17,57 +14,45 @@ def read_file(filename):
         text += line
     return text
 
-<<<<<<< HEAD
-query_file = sys.argv[1]  #file containing the query string
-doc_file = sys.argv[2]    #the document we are searching
-new_file = os.path.splitext(doc_file)[0] + "_edited.txt"
-
-query = read_file(query_file).translate(None, string.punctuation)
-text = read_file(doc_file).translate(None, string.punctuation)
-
-query_words = PunktWordTokenizer().tokenize(query)
-text_words = PunktWordTokenizer().tokenize(text)
-=======
 # Takes two command-line arguments: a question (query_file), and a document (doc_file)
 # Replaces all synonyms of words in the query that are found in the document with the
-#	original wording of the query.
+#   original wording of the query.
 # Outputs the result of this to [query_file]_edited.txt
 def main():
-	query_file = sys.argv[1]
-	doc_file = sys.argv[2]
-	new_file = os.path.splitext(doc_file)[0] + "_edited.txt"
+    query_file = sys.argv[1]
+    doc_file = sys.argv[2]
+    new_file = os.path.splitext(doc_file)[0] + "_edited.txt"
 
-	query = read_file(query_file)
-	text = read_file(doc_file)
+    query = read_file(query_file).translate(None, string.punctuation)
+    text = read_file(doc_file).translate(None, string.punctuation)
 
-	query_words = str.split(query)
-	text_words = str.split(text)
->>>>>>> f023839b7463e65b4a15b7ab293cfd1f846c4941
+    query_words = PunktWordTokenizer().tokenize(query)
+    text_words = PunktWordTokenizer().tokenize(text)
 
-	lookup = {}
+    lookup = {}
 
-	# Construct dictionary from words to synonyms in query
-	for word in query_words:
-		word = str.lower(word)
-		if word in lookup:
-			break
-		synonyms = [lemma.name for lemma in sum([ss.lemmas for ss in wordnet.synsets(word)],[])]
-		lookup[word] = synonyms
+    # Construct dictionary from words to synonyms in query
+    for word in query_words:
+        word = str.lower(word)
+        if word in lookup:
+            break
+        synonyms = [lemma.name for lemma in sum([ss.lemmas for ss in wordnet.synsets(word)],[])]
+        lookup[word] = synonyms
 
-	# traverse the document looking to replace synonyms of words in the query
-	for (i, word) in enumerate(text_words):
-		for key in lookup:
-			if str.lower(word) in lookup[key]:
-       				text_words[i] = key
-				break
+    # traverse the document looking to replace synonyms of words in the query
+    for (i, word) in enumerate(text_words):
+        for key in lookup:
+            if str.lower(word) in lookup[key]:
+                text_words[i] = key
+                break
 
-	text = " ".join(text_words)
+    text = " ".join(text_words)
 
-	if os.path.exists(new_file):
-		os.remove(new_file)
-	f = open(new_file, 'w')
-	f.write(text)
-	f.close()
+    if os.path.exists(new_file):
+        os.remove(new_file)
+    f = open(new_file, 'w')
+    f.write(text)
+    f.close()
 
 if __name__ == "__main__":
     main()
