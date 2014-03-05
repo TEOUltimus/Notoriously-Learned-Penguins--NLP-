c99e9a0f881fd16dc775d7ae9dd8d117fdcac9f2
Kyle Troutner <kyletroutner1@gmail.com>
2014-03-05 18:09:42 -0500

    fixed format of output when replacing synonyms

--- replace_synonyms.py
+++ replace_synonyms.py
@@ -6,53 +6,56 @@ import os
 import os.path
 import string
 
-# Given a filename, opens the file and returns it's text
+# Given a filename, opens the file and returns its text
 def read_file(filename):
-	f = open(filename)
-	text = ""
-	for line in f:
-		text += line
-	return text
+    f = open(filename)
+    text = ""
+    for line in f:
+        text += line
+    return text
 
-# Takes two command-line arguments: a question (query_file), and a document (doc_file)
-# Replaces all synonyms of words in the query that are found in the document with the
-#   original wording of the query.
+# Takes two command-line arguments: a question (query_file), and 
+# a document (doc_file)
+# Replaces all synonyms of words in the query that are found in the document
+# with the original wording of the query.
 # Outputs the result of this to [query_file]_edited.txt
 def main():
-	query_file = sys.argv[1]
-	doc_file = sys.argv[2]
-	new_file = os.path.splitext(doc_file)[0] + "_edited.txt"
+    query_file = sys.argv[1]
+    doc_file = sys.argv[2]
+    new_file = os.path.splitext(doc_file)[0] + "_edited.txt"
 
-	query = read_file(query_file).translate(None, string.punctuation)
-	text = read_file(doc_file).translate(None, string.punctuation)
+    query = read_file(query_file).translate(None, string.punctuation)
+    query_words = PunktWordTokenizer().tokenize(query)
 
-	query_words = PunktWordTokenizer().tokenize(query)
-	text_words = PunktWordTokenizer().tokenize(text)
+    lookup = {}
 
-	lookup = {}
+    # Construct dictionary from words to synonyms in query
+    for word in query_words:
+        word = str.lower(word)
+        if word in lookup:
+            break
+        synonyms = [lemma.name for lemma in sum([ss.lemmas for ss in wordnet.synsets(word)],[])]
+        lookup[word] = synonyms
 
-	# Construct dictionary from words to synonyms in query
-	for word in query_words:
-		word = str.lower(word)
-		if word in lookup:
-			break
-		synonyms = [lemma.name for lemma in sum([ss.lemmas for ss in wordnet.synsets(word)],[])]
-		lookup[word] = synonyms
+    output = ""
+    f = open(doc_file)
 
-	# traverse the document looking to replace synonyms of words in the query
-	for (i, word) in enumerate(text_words):
-		for key in lookup:
-			if str.lower(word) in lookup[key]:
-				text_words[i] = key
-				break
+    # traverse the document looking to replace synonyms of words in the query
+    for line in f:
+        text = line.translate(None, string.punctuation)
+        text_words = PunktWordTokenizer().tokenize(text)
+        for (i, word) in enumerate(text_words):
+            for key in lookup:
+                if str.lower(word) in lookup[key]:
+                    text_words[i] = key
+                    break
+        output += " ".join(text_words) + "\n"
 
-	text = " ".join(text_words)
-
-	if os.path.exists(new_file):
-		os.remove(new_file)
-	f = open(new_file, 'w')
-	f.write(text)
-	f.close()
+    if os.path.exists(new_file):
+        os.remove(new_file)
+    f = open(new_file, 'w')
+    f.write(output)
+    f.close()
 
 if __name__ == "__main__":
-	main()
+    main()
