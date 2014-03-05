827ad7135fcfdd108c1dc7f908ebaa8cf2d60cc9
teoultimus <esummers@andrew.cmu.edu>
2014-03-05 22:11:36 +0000

    reading in file

--- sentence_to_question.py
+++ sentence_to_question.py
@@ -5,10 +5,13 @@ def to_question(sentence):
 
 def main():
 	textfile = sys.argv[1]
+	lines = None
 	text = ''
 	with open(textfile) as f:
-		for line in f:		
-			text += line
+		lines = f.readlines()
+	for i in xrange(0, len(lines)):
+		lines[i] = lines[i].split('\n')[0]
+	text = ' '.join(lines)
 	print text
 
 if __name__=='__main__':
