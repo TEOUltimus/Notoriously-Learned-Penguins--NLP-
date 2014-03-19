24303106c4f5f1c89f0fe49b62493c4ba021269b
Kyle Troutner <kyletroutner1@gmail.com>
2014-03-19 17:59:50 -0400

    Fixed script for generating yes/no questions; still needs to check if questions are grammatical

--- sentence_to_question.py
+++ sentence_to_question.py
@@ -4,8 +4,8 @@ from stat_parser import Parser
 
 # Set up verbs, will later do pos tagging to include maybe_verbs as well
 verbs = ['be', 'am', 'are', 'is', 'was', 'were', 'being', 'could',
-	'do', 'did', 'does', 'doing', 'have', 'has', 'having',
-	'must', 'shall', 'should', 'would']
+    'do', 'did', 'does', 'doing', 'have', 'has', 'having',
+    'must', 'shall', 'should', 'would']
 maybe_verbs = ['can', 'had', 'may', 'might', 'will']
 punctuation = ['.', ',', '?', '!']
 parser = Parser()
@@ -14,54 +14,65 @@ goodQ = re.compile('\\(SBARQ')
 
 # Given a sentence, return a question if possible (and '' otherwise) 
 def to_question(sent):
-	verb = ''
-	sentence = sent.split('.')[0]
-	for i in xrange(0, len(sentence)):
-		if sentence[i] in verbs:
-			# for the first auxilliary ver found, move it to the fron of the sentence
-			verb = sentence[i]
-			sentence.remove(verb)
-			sentence.insert(0, verb)
+    verb = ''
+    sentence = (sent.split('.')[0]).split(' ')
+    for i in xrange(0, len(sentence)):
+        if sentence[i] in verbs:
+            # for the first auxilliary ver found, move it to the
+            # front  of the sentence
+            verb = sentence[i]
+            sentence.remove(verb)
+            sentence.insert(0, verb)
 
-			# replace final punctuation if any is present
-			if sentence[-1] in punctuation:
-				sentence[-1] = '?'
-			else:
-				sentence.append('?')
+            # This may cause problems with checking grammaticality
+            # replace final punctuation if any is present
+            #if sentence[-1] in punctuation:
+            #    sentence[-1] = '?'
+            #else:
+            #    sentence.append('?')
 
-			# attempt to verify prospective question is grammatical
-			sent =  ' '.join(sentence)
-			ptree = parser.parse(sent)
-			ptreenode = ptree.node
-			#print ptree
-			if goodQ.match(ptree):
-				return sent
-			else:
-				return ''
-	return ''
+            # Not working correctly at the moment
+            # attempt to verify prospective question is grammatical
+            #sent =  ' '.join(sentence)
+            #ptree = parser.parse(sent)
+            #ptreenode = ptree.node
+            #if goodQ.match(ptreenode):
+            #    return sent
+            #else:
+            #    return ''
+            return ' '.join(sentence) + '?'
+    return ''
 
 def main():
-	textfile = sys.argv[1]
-	lines = None
-	text = []
-	with open(textfile) as f:
-		lines = f.readlines()
+    textfile = sys.argv[1]
+    lines = None
+    text = []
+    with open(textfile) as f:
+        lines = f.readlines()
 
-	# remove newlines and tokenize sentences
-	for i in xrange(0, len(lines)):
-		lines[i] = lines[i].split('\n')[0].lower()
-		text += PunktSentenceTokenizer().tokenize(lines[i])
-	#text =i ' '.join(lines)
-	
-	#lines = PunktSentenceTokenizer().tokeniize(text)
+    # remove newlines and tokenize sentences
+    for i in xrange(0, len(lines)):
+        lines[i] = lines[i].split('\n')[0].lower()
+        text += PunktSentenceTokenizer().tokenize(lines[i])
+    #text =i ' '.join(lines)
+    
+    #lines = PunktSentenceTokenizer().tokeniize(text)
 
-	# list of questions to return
-	questions = []
-	for i in xrange(0, len(text)):
-		out = text[i].split(', ')
-		for line in out:
-			questions.append(to_question(PunktWordTokenizer().tokenize(line)))
-	print outs
+    # list of questions to return
+    questions = []
+    for i in xrange(0, len(text)):
+        out = text[i].split('. ')
+        for line in out:
+            question = to_question(line)
+            if question != '':
+                questions.append(question)
+            #sentences = line.split('. ')
+            #for sent in sentences:
+            #    questions.append(to_question(sent))
+            #questions.append(to_question(PunktWordTokenizer().tokenize(line)))
+    #print outs
+    for q in questions:
+        print q
 
 if __name__=='__main__':
-	main()
+    main()
