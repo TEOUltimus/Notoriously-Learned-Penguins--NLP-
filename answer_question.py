8296424138252942f91f7c8ba96a1cd5d877f581
weisbron <weisbron@gmail.com>
2014-04-02 21:12:47 -0400

    added comments to answer_question and added ds2.py, it aids in vector scoring

--- answer_question.py
+++ answer_question.py
@@ -1,27 +1,38 @@
 import ds2, re
 
+# q is the question
+# sections is a list of sections from the article
 def answer_sections(q,sections):
 	highest = 0.0
 	highestsect = 0
+	# Score each section and take the highest one
 	for i in range(0,len(sections)):
 		if ds2.compare(sections[i],q) > highest and len(sections[i].split(' ')) > 4:
 			highest = ds2.compare(sections[i],q)
 			highestsect = i
 	return sections[highestsect]
 
+# q is the question
+# sentence is a sentence from the article
 def answer_clause(q,sentence):
+	# split the sections into 'clauses' by splitting on commas (excluding dates/numbers) and periods
 	sections = re.split('(?<!\d)[,]|[.]',sentence.split('\n')[-1])
 	highest = 0.0
 	highestsect = 0
+	# score each clause and pick the highest one
 	for i in range(0,len(sections)):
 		if ds2.compare(sections[i],q) > highest and len(sections[i].split(' ')) > 4:
 			highest = ds2.compare(sections[i],q)
 			highestsect = i
 	return sections[highestsect]
 
+# q is a question as a string
+# text is the text of the wikipedia article that the question is asking about
 def answer(q,text):
+	# Break the text into both sentences and sections
 	sents = text.split('.')
 	sects = text.split('\n\n\n')
+	# Try to score by sentence and by section; pick the answer that scores the highest
 	a1 = answer_clause(q,answer_sections(q,sects))
 	a2 = answer_clause(q,answer_sections(q,sents))
 	if ds2.compare(q,a1) > ds2.compare(q,a2):
